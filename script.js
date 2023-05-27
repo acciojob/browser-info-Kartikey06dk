099c0b7dcb9d12ccc49f3a3c8bec976ccdc6d2b5
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-05-27 17:35:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-//your JS code here. If required.
-let div = document.getElementById("browser-info");
-
-let browserName = navigator.userAgent;
-let version = navigator.appVersion;
-// let version = " OPR/95.0.0.0";
-// let print = alert("You are using " + browserName)
-div.innerHTML = "You are using "   +version;
+function displayBrowserInfo() {
+      var browserInfoDiv = document.getElementById("browser-info");
+      
+      // Detect the user's browser name
+      var browserName = "";
+      if (navigator.userAgent.indexOf("Chrome") != -1)
+        browserName = "Google Chrome";
+      else if (navigator.userAgent.indexOf("Firefox") != -1)
+        browserName = "Mozilla Firefox";
+      else if (navigator.userAgent.indexOf("Safari") != -1)
+        browserName = "Apple Safari";
+      else if (navigator.userAgent.indexOf("Opera") != -1)
+        browserName = "Opera";
+      else if (navigator.userAgent.indexOf("Edge") != -1)
+        browserName = "Microsoft Edge";
+      else if (navigator.userAgent.indexOf("Trident") != -1)
+        browserName = "Internet Explorer";
+      else
+        browserName = "Unknown";
+      
+      // Get the browser version
+      var version = parseFloat(navigator.appVersion);
+      
+      // Display the browser information
+      var infoMessage = "You are using " + browserName + " version " + version;
+      browserInfoDiv.textContent = infoMessage;
+    }
